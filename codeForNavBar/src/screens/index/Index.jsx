c36eb964501210a4eb712ee196dedef20590614a
Nicholas Amancio <nick.amancio44@gmail.com>
2023-12-27 21:20:07 -0800

    got code online. I think this should work. perchance

---
+++ codeForNavBar/src/screens/index/Index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./style.css";
+
+export const Index = () => {
+  return (
+    <div className="index">
+      <div className="overlap-group-wrapper">
+        <div className="overlap-group">
+          <div className="text-wrapper">SEARCH</div>
+          <div className="div">LEADERBOARD</div>
+          <div className="rectangle" />
+          <div className="text-wrapper-2">BROWSE</div>
+          <div className="text-wrapper-3">LOGIN</div>
+          <div className="text-wrapper-4">WEBSITE TITLE</div>
+          <div className="navbar-background" />
+          <div className="rectangle-2" />
+          <div className="rectangle-3" />
+          <div className="rectangle-4" />
+        </div>
+      </div>
+    </div>
+  );
+};
